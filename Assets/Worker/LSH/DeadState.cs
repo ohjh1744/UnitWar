a22c18b1697448b290eaecd512d8f49fbf6d2ffe
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-11-24 20:50:26 +0900

    [FEAT} Dead 쪽 setactive false 처리.

--- Assets/Worker/LSH/DeadState.cs
+++ Assets/Worker/LSH/DeadState.cs
@@ -33,9 +33,7 @@ public class DeadState : MonoBehaviour, IState
     public void Dead()
     {
         Debug.Log("Dead 상태 (죽음)");
-        //못움직이고
-        //애니메이션 추가
-        //오브젝트 사라짐
+        _unitController.gameObject.SetActive(false);
     }
 
 
